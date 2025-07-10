eacd00b7eac2f963e19f5b134ddedce10b9f5bda
AdrianSarmiento123 <henryadriansarmiento@gmail.com>
2025-07-10 15:56:55 -0500

    Update and rename V5__modificar_longitud_nombre.sql to V5__asignar_roles_a_usuarios.sql

---
+++ mi-demo-flyway/sql/V5__asignar_roles_a_usuarios.sql
@@ -0,0 +1,3 @@
+UPDATE usuarios SET rol_id = 1 WHERE email = 'juan@example.com';
+UPDATE usuarios SET rol_id = 2 WHERE email = 'laura@example.com';
+UPDATE usuarios SET rol_id = 3 WHERE email = 'carlos@example.com';
--- mi-demo-flyway/sql/V5__modificar_longitud_nombre.sql
+++
@@ -1,3 +0,0 @@
--- Supongamos que ahora queremos permitir nombres más largos
-ALTER TABLE usuarios
-ALTER COLUMN nombre TYPE VARCHAR(200);
